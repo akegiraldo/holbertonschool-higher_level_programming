33ae5787caeec567e53bfeb696185d5a63e5ea0f
Doouh <ake.giraldo@hotmail.com>
2019-12-10 12:34:19 -0500

    Fix task 1 answer

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,10 +11,7 @@ if (cp % 10) > 5:
     else:
         print("{:d} and is greater than 5".format(cp % 10))
 elif (cp % 10) == 0:
-    if number < 0:
-        print("-{:d} and is 0".format(cp % 10))
-    else:
-        print("{:d} and is 0".format(cp % 10))
+    print("{:d} and is 0".format(cp % 10))
 elif (cp % 10) < 6:
     if number < 0:
         print("-{:d} and is less than 6 and not 0".format(cp % 10))
