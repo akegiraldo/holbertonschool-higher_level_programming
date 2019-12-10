c3dde58d013134dc30fe1ef5c34c4063b8f42c1a
Doouh <ake.giraldo@hotmail.com>
2019-12-10 12:24:59 -0500

    Fix task 1 answer

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,19 +3,20 @@ import random
 number = random.randint(-10000, 10000)
 cp = number
 if (cp < 0):
-	cp *= -1
+    cp *= -1
+print('Last digit of {:d} is '.format(number), end='')
 if (cp % 10) > 5:
     if number < 0:
-        print("Last digit of {:d} is -{:d} and is greater than 5".format(number, cp % 10))
+        print("-{:d} and is greater than 5".format(cp % 10))
     else:
-        print("Last digit of {:d} is {:d} and is greater than 5".format(number, cp % 10))
+        print("{:d} and is greater than 5".format(cp % 10))
 elif (cp % 10) == 0:
     if number < 0:
-        print("Last digit of {:d} is -{:d} and is 0".format(number, cp % 10))
+        print("-{:d} and is 0".format(cp % 10))
     else:
-        print("Last digit of {:d} is {:d} and is 0".format(number, cp % 10))
+        print("{:d} and is 0".format(cp % 10))
 elif (cp % 10) < 6:
     if number < 0:
-        print("Last digit of {:d} is -{:d} and is less than 6 and not 0".format(number, cp % 10))
+        print("-{:d} and is less than 6 and not 0".format(cp % 10))
     else:
-        print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, cp % 10))
+        print("{:d} and is less than 6 and not 0".format(cp % 10))
