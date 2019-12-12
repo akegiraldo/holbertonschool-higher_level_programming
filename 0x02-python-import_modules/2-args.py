f3f19b96c4adb6f48568e92fa0d89e67f883bcba
Doouh <ake.giraldo@hotmail.com>
2019-12-12 12:43:48 -0500

    Fix pep8 in task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(argv) == 1:
         print("0 arguments.")
     elif len(argv) == 2:
@@ -9,5 +9,5 @@ if __name__ == "__main__":
         print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(len(argv) - 1))
-        for i in range (1, len(argv)):
+        for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
