567ea621ec3d09056a7724aaeb7e57587d210433
Doouh <ake.giraldo@hotmail.com>
2019-12-12 09:30:30 -0500

    Add dir and files

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == '__main__':
+    length = len(argv) - 1
+    acu = 0
+    if length == 0:
+        print("0")
+    else:
+        for i in range(1, length + 1):
+           acu += int(argv[i])
+        print("{}".format(acu))
