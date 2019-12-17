52638053f35392fbca63702284db4b158e09a3a7
Doouh <ake.giraldo@hotmail.com>
2019-12-17 10:10:12 -0500

    Add until task 8 answers

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    newstr = ''
+    for i in (my_string):
+        if (chr(ord(i)) == 'c' or chr(ord(i)) == 'C'):
+            continue
+        else:
+            newstr += chr(ord(i))
+    return newstr
