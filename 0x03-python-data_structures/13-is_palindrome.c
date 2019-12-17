1053b5f9130c560e2cb2c17716b710df6ac60792
Doouh <ake.giraldo@hotmail.com>
2019-12-17 18:31:19 -0500

    Fix task answer

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,19 +9,21 @@
 
 int is_palindrome(listint_t **head)
 {
-	int vector[1000], i = 0, j = 0;
+	int vector[1000], i = 0, j = 0, t = 0;
 	listint_t *tmp;
 
 	tmp = *head;
 	while (tmp)
 		vector[i] = tmp->n, i++, tmp = tmp->next;
 	i--;
-	while (j < (i / 2))
+	if (i % 2 != 0)
+		t = i - 1 / 2;
+	else
+		t = i / 2;
+	while (j < t)
 	{
 		if (vector[j] != vector[i])
-		{
 			return (0);
-		}
 		i--;
 		j++;
 	}
