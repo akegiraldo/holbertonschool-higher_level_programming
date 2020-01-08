ceaaedf0e0aa3207489936baa8603738f8e9e2c8
Doouh <ake.giraldo@hotmail.com>
2020-01-08 13:45:17 -0500

    Fix problem with double inclusion line

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
-    def safe_print_list(my_list=[], x=0):
-        j = 0
-        for i in range(x):
-            try:
-                print("{}".format(my_list[i]), end='')
-                j += 1
-            except IndexError:
-                print("", end='')
-        print("")
-        return (j)
+def safe_print_list(my_list=[], x=0):
+    j = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+            j += 1
+        except IndexError:
+            print("", end='')
+    print("")
+    return (j)
