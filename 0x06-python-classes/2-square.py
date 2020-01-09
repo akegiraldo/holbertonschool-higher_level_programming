4a6aa073512c84f89934b412f784307b2d433ea4
Doouh <ake.giraldo@hotmail.com>
2020-01-09 17:57:32 -0500

    Add dir, some tasks and readme

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square():
+    
+    def __init__(self, size=0):
+        a = 0
+        try:
+            a += size
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        except TypeError:
+            raise TypeError("size must be an integer")
