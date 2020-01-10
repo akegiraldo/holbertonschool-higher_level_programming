003bc62567630d6e0eb85f906930652e41d968e3
Doouh <ake.giraldo@hotmail.com>
2020-01-09 19:27:11 -0500

    Add all

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,8 +3,9 @@
 
 class Square():
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         self.size = size
+        self.position = position
 
     def area(self):
         return self.__size * self.__size
@@ -23,8 +24,20 @@ class Square():
 
     def my_print(self):
         for i in range(self.__size):
+            for x in range(self.__position[0]):
+                print("")
             for j in range(self.__size):
                 print("#", end='')
             print("")
         if (self.__size == 0):
-            print(""position must be a tuple of 2 positive integers)
+            print("")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not type(value[0]) == int and type(value[1]) == int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__posiition = value
