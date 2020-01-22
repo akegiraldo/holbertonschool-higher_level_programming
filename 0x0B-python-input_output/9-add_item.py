324db1d0c5e70c841073e36c2a7f9ff19ff9f933
Kevin Adrian Giraldo Valderrama <30760255+Doouh@users.noreply.github.com>
2020-01-21 22:54:38 -0500

    Update 9-add_item.py

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
-import json
-"""
-adds all arguments to a Python list, and then save them to a file
-"""
+import sys
 
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
+filename = 'add_item.json'
 try:
-    list_to_json = load_from_json_file("add_item.json")
-except:
-        list_to_json = []
-
-for i in range(1, len(argv)):
-    list_to_json.append(argv[i])
-
-save_to_json_file(list_to_json, "add_item.json")
+    ls = load_from_json_file(filename)
+except Exception:
+    ls = []
+for i in range(1, len(sys.argv)):
+    ls.append(sys.argv[i])
+save_to_json_file(ls, filename)
