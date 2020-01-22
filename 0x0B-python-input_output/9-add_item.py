62a0b1d891cada016e2d22e323c4b4a2beae28aa
Kevin Adrian Giraldo Valderrama <30760255+Doouh@users.noreply.github.com>
2020-01-21 22:52:30 -0500

    Update 9-add_item.py

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-import json
 from sys import argv
+import json
+"""
+adds all arguments to a Python list, and then save them to a file
+"""
 
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
