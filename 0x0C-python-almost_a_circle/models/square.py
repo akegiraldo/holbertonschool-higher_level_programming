36a1fba1d6a55810b5705071df1f16137e433925
Doouh <ake.giraldo@hotmail.com>
2020-01-27 17:22:47 -0500

    Add functions

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,3 +13,29 @@ class Square(Rectangle):
         """ --- """
         return "[" + __class__.__name__ + "] (" + str(self.id) + ") " +\
                str(self.x) + "/" + str(self.y) + " - " + str(self.width)
+
+    def update(self, *args, **kwargs):
+        """ --- """
+        if len(args) > 0:
+            i = 0
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                elif i == 1:
+                    self.width = args[i]
+                    self.height = args[i]
+                elif i == 2:
+                    self.x = args[i]
+                elif i == 3:
+                    self.y = args[i]
+        else:
+            for key in kwargs:
+                if key == "id":
+                    self.id = kwargs[key]
+                elif key == "size":
+                    self.width = kwargs[key]
+                    self.height = kwargs[key]
+                elif key == "x":
+                    self.x = kwargs[key]
+                elif key == "y":
+                    self.y = kwargs[key]
