db35858e9876e85356f610d380f9f13fe6f8d28e
Doouh <ake.giraldo@hotmail.com>
2020-03-02 17:19:12 -0500

    Fix tas 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,6 @@
 -- Script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SHOW CREATE TABLE;
+SHOW (CREATE TABLE IF NOT EXISTS first_table
+(
+        id INT,
+        name VARCHAR(256)
+););
