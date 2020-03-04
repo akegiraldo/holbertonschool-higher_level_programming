5d559010e7211850fa025102123b3ff90e50d66e
Kevin Adrian Giraldo Valderrama <30760255+Doouh@users.noreply.github.com>
2020-03-03 22:39:06 -0500

    Update 15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,1 +1,2 @@
 -- Script that lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT t_s.title FROM tv_shows t_s INNER JOIN tv_show_genres t_s_g ON t_s.id = t_s_g.show_id INNER JOIN tv_genres t_g ON t_s_g.genre_id = t_g.id WHERE t_g.name = 'Comedy' ORDER BY t_s.title;
