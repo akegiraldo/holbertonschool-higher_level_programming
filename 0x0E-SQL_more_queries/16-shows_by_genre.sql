5c59b3b436a4e67a025364bdfc51acdfc4ea4209
Kevin Adrian Giraldo Valderrama <30760255+Doouh@users.noreply.github.com>
2020-03-03 22:41:52 -0500

    Update 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,1 +1,2 @@
 -- Script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+SELECT t_s.title, t_g.name FROM tv_shows t_s LEFT OUTER JOIN tv_show_genres t_s_g ON t_s.id = t_s_g.show_id LEFT OUTER JOIN tv_genres t_g ON t_s_g.genre_id = t_g.id ORDER BY t_s.title, t_g.name;
