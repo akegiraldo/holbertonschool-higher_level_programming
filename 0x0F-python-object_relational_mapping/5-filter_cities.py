196b9cc9a5c809fb404eff2c31765202a7d1792b
Doouh <ake@giraldo.com>
2020-03-28 17:48:37 +0000

    Fix task 1 and 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,19 +2,16 @@
 # Use MySQLdb with python3
 
 if __name__ == '__main__':
-	import MySQLdb
-	from sys import argv
+        import MySQLdb
+        from sys import argv
 
-	db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-	cur = db.cursor()
-	size = cur.execute("SELECT cities.name FROM cities, states WHERE cities.state_id = states.id AND states.name = %s ORDER BY cities.id;", (argv[4], ))
-	rows = cur.fetchall()
-	i = 0
-	for row in rows:
-		if i < size - 1:
-			print(row[0], end=", ")
-		else:
-			print(row[0])
-		i += 1
-	cur.close()
-	db.close()
+        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                             passwd=argv[2], db=argv[3])
+        cur = db.cursor()
+        cur.execute("SELECT cities.name FROM cities, states WHERE \
+                     cities.state_id = states.id AND states.name = %s \
+                     ORDER BY cities.id;", (argv[4], ))
+        rows = cur.fetchall()
+        print(", ".join([row[0] for row in rows]))
+        cur.close()
+        db.close()
