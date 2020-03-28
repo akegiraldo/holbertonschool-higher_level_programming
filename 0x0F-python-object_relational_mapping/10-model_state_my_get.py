2611ecd36d189d8a8d0b98db9a6438913df47d1e
Doouh <ake@giraldo.com>
2020-03-28 16:29:06 +0000

    Add tasks until 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# Lists all State objects from the database hbtn_0e_6_usa
+
+if __name__ == '__main__':
+	from model_state import Base, State
+	from sqlalchemy.orm import sessionmaker
+	from sqlalchemy import (create_engine)
+	from sys import argv
+
+	engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+	Session = sessionmaker(bind=engine)
+	Session.configure(bind=engine)
+	session = Session()
+
+	for row in session.query(State).order_by(State.id).filter(State.name == argv[4]):
+		if row:
+			print(row.id)
+		else:
+			print("Not found")
+	
