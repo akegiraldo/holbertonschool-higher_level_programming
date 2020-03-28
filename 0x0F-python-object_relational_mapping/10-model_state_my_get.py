a4c5c16903fe9def18f9b51ec63bfa97805312e3
Doouh <ake@giraldo.com>
2020-03-28 17:27:18 +0000

    Fix pep8

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -2,21 +2,23 @@
 # Lists all State objects from the database hbtn_0e_6_usa
 
 if __name__ == '__main__':
-	from model_state import Base, State
-	from sqlalchemy.orm import sessionmaker
-	from sqlalchemy import (create_engine)
-	from sys import argv
+        from model_state import Base, State
+        from sqlalchemy.orm import sessionmaker
+        from sqlalchemy import (create_engine)
+        from sys import argv
 
-	engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                               argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-	Session = sessionmaker(bind=engine)
-	Session.configure(bind=engine)
-	session = Session()
+        Session = sessionmaker(bind=engine)
+        Session.configure(bind=engine)
+        session = Session()
 
-	size = session.query(State).order_by(State.id).filter(State.name == argv[4]).count()
-	
-	if size > 0:
-		for row in session.query(State).order_by(State.id).filter(State.name == argv[4]):
-			print(row.id)
-	else:
-		print("Not found")	
+        size = session.query(State).order_by(State.id).filter(
+               State.name == argv[4]).count()
+        if size > 0:
+                for row in session.query(State).order_by(State.id).filter(
+                           State.name == argv[4]):
+                        print(row.id)
+        else:
+                print("Not found")
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -2,16 +2,18 @@
 # Lists all State objects from the database hbtn_0e_6_usa
 
 if __name__ == '__main__':
-	from model_state import Base, State
-	from sqlalchemy.orm import sessionmaker
-	from sqlalchemy import (create_engine)
-	from sys import argv
+        from model_state import Base, State
+        from sqlalchemy.orm import sessionmaker
+        from sqlalchemy import (create_engine)
+        from sys import argv
 
-	engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                               argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-	Session = sessionmaker(bind=engine)
-	Session.configure(bind=engine)
-	session = Session()
+        Session = sessionmaker(bind=engine)
+        Session.configure(bind=engine)
+        session = Session()
 
-	for row in session.query(State).order_by(State.id).filter(State.name.like('%a%')):
-		print("{}: {}".format(row.id, row.name))
+        for row in session.query(State).order_by(State.id).filter(
+                                 State.name.like('%a%')):
+                print("{}: {}".format(row.id, row.name))
