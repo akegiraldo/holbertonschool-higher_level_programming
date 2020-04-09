faad542742b06a19afb9c4a04093dd1cac396d9c
Doouh <ake.giraldo@hotmail.com>
2020-04-09 15:07:55 -0500

    Add task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s "$1" -X POST -d "$2"
+curl -H "Content-type: application/json" -s "$1" -X POST -d @"$2"
