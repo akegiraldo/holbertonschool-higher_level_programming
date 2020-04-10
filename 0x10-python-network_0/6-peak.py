b7974e85f396e8d4e1a55bb090bc4abbfb0a586d
Doouh <ake.giraldo@hotmail.com>
2020-04-10 14:39:19 -0500

    Fix conditional at task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+
 def find_peak(list_of_integers):
-    list_of_integers.sort()
-    return list_of_integers[-1]
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
