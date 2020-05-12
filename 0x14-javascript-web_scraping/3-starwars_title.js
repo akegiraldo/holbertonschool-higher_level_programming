edba3c95e184f8809268633be912c4771e9efe30
Doouh <ake.giraldo@hotmail.com>
2020-05-12 12:51:28 -0500

    The same but another way

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,6 +4,10 @@
 const request = require('request');
 
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
-  if (err) { console.log(err); return; }
-  console.log(JSON.parse(body).title);
+  if (err) {
+    console.log(err);
+  } else {
+    const info = JSON.parse(body);
+    console.log(info.title);
+  }
 });
