4039a0bac4b5beddeb92592ce2276a9d59f020a1
Doouh <ake.giraldo@hotmail.com>
2020-05-12 13:32:54 -0500

    7 validation

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,11 +3,15 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    const info = JSON.parse(body);
-    console.log(info.title);
-  }
-});
+if (process.argv[2] === '7') {
+  console.log(undefined);
+} else {
+  request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      const info = JSON.parse(body);
+      console.log(info.title);
+    }
+  });
+}
