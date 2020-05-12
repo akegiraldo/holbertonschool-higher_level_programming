847315593b16a6a540168e9dfc8d9f4c30179244
Doouh <ake.giraldo@hotmail.com>
2020-05-12 12:50:20 -0500

    Fix url

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,10 +2,8 @@
 // Script that prints the title of a Star Wars movie where the episode number matches a given integer.
 
 const request = require('request');
-const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
 
-request(`${url}${id}`, function (err, response, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
   if (err) { console.log(err); return; }
-  if (JSON.parse(body).title !== undefined) { console.log(JSON.parse(body).title); } else { console.log(JSON.parse(body)); }
+  console.log(JSON.parse(body).title);
 });
