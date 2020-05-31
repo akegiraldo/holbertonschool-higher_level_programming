972be42f90a3c14cf237f406e20ddad9885128db
Doouh <ake.giraldo@hotmail.com>
2020-05-31 10:10:57 -0500

    Reduce algorthm

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,10 +2,4 @@
 
 
 def search_replace(my_list, search, replace):
-    new = []
-    for i in my_list:
-        if i == search:
-            new.append(replace)
-        else:
-            new.append(i)
-    return new
+    return [replace if i == search else i for i in my_list]
