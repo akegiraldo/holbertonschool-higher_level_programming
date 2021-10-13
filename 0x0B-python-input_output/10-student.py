b5449d09cf1066d21ea9884df50506819a3f0fd9
Kevin Adrian Giraldo Valderrama <30760255+akegiraldo@users.noreply.github.com>
2021-10-12 19:57:07 -0500

    Add method to retrieve just some attrs

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attributes=None):
+        all_attributes = self.__dict__.copy()
+
+        if attributess and isinstance(attributes, list):
+            for key in self.__dict__.keys():
+                if key not in attributes:
+                    del all_attributes[key]
+        return all_attributes
