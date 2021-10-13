a10f9ff50d28a8b756d7a178fa7ec5db711c7f6e
Kevin Adrian Giraldo Valderrama <30760255+akegiraldo@users.noreply.github.com>
2021-10-12 20:01:34 -0500

    Update file

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-
+"""
+    student file
+"""
 
 class Student:
+    """Class student"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
